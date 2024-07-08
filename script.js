fed6183cb671dd0d1869d99bad9cd33597c78821
Bala Vamsi Maragani <mbalavamsi26@gmail.com>
2024-07-07 22:40:48 -0400

    code changes
    Added redirection action page post submitting the contact form; fixed bugs.

--- script.js
+++ script.js
@@ -13,8 +13,26 @@ document
       return;
     }
 
-    // Perform form submission logic here, such as sending an email or storing the data in a database.
+    // Prepare the data to be sent
+    var formData = new FormData(this);
 
-    alert("Form submitted successfully!");
+    fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      body: formData,
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.success) {
+          // Redirect to success page
+          window.location.href = "success.html";
+        } else {
+          alert("Form submission failed. Please try again.");
+        }
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+        alert("An error occurred. Please try again.");
+      });
+
     this.reset();
   });
